ddc9751dd05f16a275cdb46cf638aef8a6026e69
carlosrenatosouza2 <carlos.renato.souza@gmail.com>
2025-08-06 17:36:21 -0300

    adicionei os scripts da pasta Work/ATLSW12/wr_17022023/ no repositorio: modified:   .gitignore new file:   .gitignore_main new file:   Work/ATLSW12/wr_17022023/limpa.sh new file:   Work/ATLSW12/wr_17022023/link.sh new file:   Work/ATLSW12/wr_17022023/run.ksh new file:   Work/ATLSW12/wr_17022023/run_atlsw12_WR.sh new file:   Work/ATLSW12/wr_17022023/run_atlsw12_WR_gnu.sh

---
+++ Work/ATLSW12/wr_17022023/link.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+ln -sf VEGPARM.TBL  ../../../WRF/run/VEGPARM.TBL .
+ln -sf URBPARM.TBL  ../../../WRF/run/URBPARM.TBL .
+ln -sf SOILPARM.TBL ../../../WRF/run/SOILPARM.TBL .
+ln -sf LANDUSE.TBL ../../../WRF/run/LANDUSE.TBL .
+ln -sf GENPARM.TBL  ../../../WRF/run/GENPARM.TBL .
+ln -sf RRTM_DATA_DBL  ../../../WRF/run/RRTM_DATA_DBL .
+ln -sf RRTM_DATA  ../../../WRF/run/RRTM_DATA .
+ln -sf RRTM_DATA_DBL  ../../../WRF/run/RRTM_DATA_DBL .
+ln -sf RRTM_DATA ../../../WRF/run/RRTM_DATA .
+ln -sf RRTMG_SW_DATA ../../../WRF/run/RRTMG_SW_DATA .
+ln -sf RRTMG_LW_DATA_DBL ../../../WRF/run/RRTMG_LW_DATA_DBL .
+ln -sf RRTMG_LW_DATA ../../../WRF/run/RRTMG_LW_DATA .
+ln -sf RRTMG_SW_DATA_DBL ../../../WRF/run/RRTMG_SW_DATA_DBL .
+ln -sf ozone_plev.formatted ../../../WRF/run/ozone_plev.formatted .
+ln -sf ozone_lat.formatted ../../../WRF/run/ozone_lat.formatted .
+ln -sf ozone.formatted ../../../WRF/run/ozone.formatted .
+ln -sf tr49t67 ../../../WRF/run/tr49t67 .
+ln -sf tr49t85 ../../../WRF/run/tr49t85 .
+ln -sf tr67t85 ../../../WRF/run/tr67t85 .
+ln -sf ETAMPNEW_DATA ../../../WRF/run/ETAMPNEW_DATA .
+ln -sf ETAMPNEW_DATA_DBL ../../../WRF/run/ETAMPNEW_DATA_DBL .
+ln -sf ETAMPNEW_DATA.expanded_rain ../../../WRF/run/ETAMPNEW_DATA.expanded_rain .
+ln -sf ETAMPNEW_DATA.expanded_rain_DBL ../../../WRF/run/ETAMPNEW_DATA.expanded_rain_DBL .
+
+
---
+++ Work/ATLSW12/wr_17022023/run_atlsw12_WR.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#SBATCH --job-name=Controle           # nome do job
+#SBATCH --partition=batch                # fila que vai submeter
+#SBATCH --ntasks=256                    # Total de cores (nodes*tasks)
+###SBATCH --nodes=2                       # quantidade de nós
+###SBATCH --tasks-per-node=128             # quantidade de cores por nó
+#SBATCH --time=08:00:00                  #tempo para rodar
+#SBATCH --output=/mnt/beegfs/carlos.souza/Doutorado/COAWST.V3.7/Work/ATLSW12/wr_17022023/rws1_total.out
+#SBATCH --exclusive
+#
+# Script to run COAWST in a given Work directory
+# Luciano Pezzi - February 2022
+#
+  echo "Running COAWST on EGEON"
+  echo "Rodando em ${ntasks} CORES"
+#
+# Machine defifnitions for CRAY
+#
+  export MPICH_ENV_DISPLAY=1
+  export MPICH_ABORT_ON_ERROR=1
+  export MPICH_RANK_REORDER_DISPLAY=1
+  export OMP_NUM_THREADS=1
+#
+# Root, Project and work directories definition
+#  
+  MODEL=ATLSW12
+  COAVER=COAWST.V3.7
+  EXPER=wr_17022023
+  
+  ROOTDIR=/mnt/beegfs/carlos.souza/Doutorado/${COAVER}
+  WORKDIR=/mnt/beegfs/carlos.souza/Doutorado/COAWST.V3.7
+  PROJDIR=${ROOTDIR}/Projects/${MODEL}/${EXPER}
+  EXECDIR=${WORKDIR}/Work/${MODEL}/${EXPER}
+  echo '${PROJDIR}='${PROJDIR}
+  echo '${EXECDIR}='${EXECDIR}
+# 
+
+  cd $SLURM_SUBMIT_DIR
+
+#. ${WORKDIR}/config_env_intel_egeon.ksh
+#. ${WORKDIR}/config_env_gnu_egeon.ksh
+#. ${WORKDIR}/config_env_gnu_egeon_mylibs.ksh
+
+
+
+. ${WORKDIR}/config_env_gnu_egeon_gcc+mpichspack_libsJP.ksh
+
+
+
+ echo "Lista de módulos carregados: "
+ module list
+ echo "============================="
+
+# 
+# Copying input files for WRF
+#
+./link.sh
+\cp -f ${ROOTDIR}/LANDUSE.TBL .
+\cp -f ${ROOTDIR}/GENPARM.TBL .
+\cp -f ${ROOTDIR}/VEGPARM.TBL .
+\cp -f ${ROOTDIR}/URBPARM.TBL . 
+\cp -f ${ROOTDIR}/SOILPARM.TBL .
+\cp -f ${ROOTDIR}/RRTM_DATA_DBL . 
+\cp -f ${ROOTDIR}/RRTM_DATA .
+\cp -f ${ROOTDIR}/WRF/run/RRTMG* .
+
+#\cp -f -r ${PROJDIR}/200* .
+\cp -f ${PROJDIR}/swa12_*.nc .
+\cp -f ${PROJDIR}/namelist.input .
+\cp -f ${PROJDIR}/roms_ini_*.nc .
+\cp -f ${PROJDIR}/roms_bry_*.nc .
+\cp -f ${PROJDIR}/wrflowinp_d0* .
+\cp -f ${PROJDIR}/wrfbdy_d0* .
+\cp -f ${PROJDIR}/wrfinput_d0* .
+\cp -f ${PROJDIR}/coupling_ATLSW12.in .
+\cp -f ${PROJDIR}/ocean_ATLSW12.in .
+#\cp -f ${PROJDIR}/*swan* .
+#\cp -f ${PROJDIR}/swan* .
+\cp -f ${PROJDIR}/scrip_wrs_static.nc .
+\cp -f ${ROOTDIR}/coawstM .
+#
+# sleep 30
+#
+  chmod 755 *
+
+ulimit -s unlimited
+MPI_PARAMS="-iface ib0 -bind-to core -map-by core"
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export I_MPI_DEBUG=5
+export MKL_DEBUG_CPU_TYPE=5
+#
+# Submitting Job
+#
+date
+#time /opt/spack/opt/spack/linux-rhel8-zen2/gcc-9.4.0/mpich-4.0.2-gpof2pvzizt3kv5rqkn4jmwakp7n6uk7/bin/mpirun -n ${SLURM_NTASKS} ./coawstM ./coupling_ATLSW12.in &> rws.out
+#time mpirun -n ${SLURM_NTASKS} -env MKL_DEBUG_CPU_TYPE=5 -env UCX_NET_DEVICES=mlx5_0:1 -genvall ./coawstM ./coupling_ATLSW12.in &> rws.out
+time mpirun -n ${SLURM_NTASKS} -genvall -env UCX_NET_DEVICES=mlx5_0:1 ./coawstM ./coupling_ATLSW12.in &> rws.out
+#srun --nodes=2 --ntasks=256 --cpus-per-task=128 ./coawstM ./coupling_ATLSW12.in &> rws.out
+date
